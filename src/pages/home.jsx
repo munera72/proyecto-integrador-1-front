61d3e4a25eaf3bb10e81431e295d789aeeb0ef86
Emanuel M <emanuel.munera1@gmail.com>
2025-04-09 14:02:29 -0500

    Started homepage

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Navbar from '../components/navbar';
 
 const Home = () => {
     return (
-        <div>
-            This is home
+        <div class="container">
+            <Navbar></Navbar>
+                <div class="main">
+                    <h1>Bienvenido al sistema de<br/>detección de nanomateriales</h1>
+                    <button class="start-btn">Iniciar proceso</button>
+                </div>
         </div>
     );
 }
