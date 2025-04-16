31b0acf40aef29a81d7699c57470f686ac61c8f5
RockSkull <sebastianpz122@gmail.com>
2025-04-16 11:01:48 -0500

    Results page modified, block components remove and embed pdf created

--- src/pages/results.jsx
+++ src/pages/results.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import Navbar from '../components/navbar';
-import ContentBlock from '../components/contentblock';
-import diamante from '../assets/Diamond_Ore.png';
-import titanio from '../assets/Titanio.png'
 
 
 function Results(){
@@ -13,14 +10,8 @@ function Results(){
             <div className='navbar'>
                 <div className="logo">Resultados del analisis</div>
             </div>
-            <div className='content-row'>
-                <ContentBlock imageSrc={diamante}/>
-                <ContentBlock imageSrc={titanio}/>
-                <ContentBlock text="Resutados del analisis"/>
-            </div>
-            <div className='main-block'>
-                    <button className="btn primary-btn">Descargar resultados</button>
-            </div>
+            <hr />
+            <embed src="/Prueba.pdf" width="1535" height="1000" type="application/pdf" />
         </div>
         )
     }
