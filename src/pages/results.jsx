8c87fd5df83dfff21117b79d01e081f8f72d08b0
RockSkull <sebastianpz122@gmail.com>
2025-05-12 12:18:59 -0500

    funtion get created; button to trigger the download

--- src/pages/results.jsx
+++ src/pages/results.jsx
@@ -3,6 +3,31 @@ import Navbar from '../components/navbar';
 
 
 function Results(){
+    
+     const handleDownload = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/images/download/", {
+        method: "GET",
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "zip.zip"; // Optionally get this dynamically
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error("Download failed:", error);
+    }
+  };
+    
     return(
         <div className="container">
             <Navbar/>
@@ -13,7 +38,7 @@ function Results(){
             <hr />
             <embed src="/Prueba.pdf" width="auto" height="4000" type="application/pdf" />
                 <div className='main'>
-                <button className='btn primary-btn'>Descargar imagenes .zip</button>
+                <button className='btn primary-btn' onClick={handleDownload}>Descargar imagenes .zip</button>
             </div>
         </div>
         )
